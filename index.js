39fd95211dbefce71ebd06eb25fa86cf79b31e63
ssaumya1711 <ssaumya1711@gmail.com>
2022-01-03 23:09:00 +0530

    changed-1 app.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors');
 const mongodb = require('mongodb');
 const mongoClient = mongodb.MongoClient;
-const url = "mongodb://localhost:27017";
+const url = "mongodb+srv://Saumya:mini171100@cluster0.ovdbg.mongodb.net?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
